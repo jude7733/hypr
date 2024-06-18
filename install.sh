a8d312913f44f0f30662951d43a1139acd21bd5b
jude7733 <judeputhussery360@gmail.com>
2024-06-18 20:13:49 +0530

    updated install script with yay and oh-my-zsh plugins

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 sudo pacman -Syu
 # add nerdfonts for neovim and configure kitty
 
+# install yay
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ~
+rm -rf yay
+yay -S wlogout
+
 sudo pacman -S ccmake wireplumbler lxappearance nwg-look arc-gtk-theme adapta-gtk-theme breeze-gtk thunar adwaita-icon-theme arc-icon-theme ristretto
 
 # notification daemon 
@@ -19,13 +28,17 @@ sudo pacman -S polkit-kde-agent
 sudo pacman -S qt5-wayland qt6-wayland
 
 # hyprland plugins and packages
-sudo pacman -S cpio meson hyprpaper waybar hypridle hyprlock
+sudo pacman -S cpio meson hyprpaper hyprshot waybar hypridle hyprlock
 
 # enable packages
 hyprpm update
 hyprpm add https://github.com/hyprwm/hyprland-plugins
+hyprpm enable hyprexpo
 
 # zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
+git clone https://github.com/hlissner/zsh-autopair ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autopair
+git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
