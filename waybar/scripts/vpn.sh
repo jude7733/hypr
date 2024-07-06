95e9b48f38d800e7b01e81e4e64d069cae01a7a4
jude7733 <judeputhussery360@gmail.com>
2024-07-06 11:57:13 +0530

    moved out waybar, install script configs
    moved to .config in dotfiles repo

--- waybar/scripts/vpn.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ip route | grep -q 'proton0' \
-&& echo '{"text":"Connected","class":"connected","percentage":100}' \
-|| echo '{"text":"Disconnected","class":"disconnected","percentage":0}'
