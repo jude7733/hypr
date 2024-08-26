2538af2766b0b135c5e793f9be196860438028fb
jude7733 <judeputhussery360@gmail.com>
2024-08-26 18:38:42 +0530

    enable generating 16 colors
    added rules

--- scripts/pywall.sh
+++ scripts/pywall.sh
@@ -2,6 +2,6 @@
 
 wallpaper="$1"
 
-sleep 2 && wal -i "$wallpaper" && xrdb -merge ~/.cache/wal/colors.Xresources &
+sleep 2 && wal --cols16 -i "$wallpaper" && xrdb -merge ~/.cache/wal/colors.Xresources &
 
 sleep 3 && pkill waybar && waybar &
