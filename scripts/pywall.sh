161810264fd820fbd48a17b957d8f38fe646bdb6
jude7733 <judeputhussery360@gmail.com>
2024-09-20 20:01:50 +0530

    added binds, rules

--- scripts/pywall.sh
+++ scripts/pywall.sh
@@ -2,17 +2,17 @@
 
 wallpaper="$1"
 
-sleep 1 && echo -e "\nGenerating ${bold}pywal colorscheme from $wallpaper" &&
+sleep 2 && echo -e "\nGenerating ${bold}pywal colorscheme from $wallpaper" &&
 wal --cols16 -i "$wallpaper" && xrdb -merge ~/.cache/wal/colors.Xresources &
 
-sleep 2 && echo -e "\nSetting colors for ${bold}btop" &&
+sleep 3 && echo -e "\nSetting colors for ${bold}btop" &&
 yes | cp -v ~/.cache/wal/colors-btop.theme ~/.config/btop/themes && pkill btop && 
 hyprctl dispatch exec '[workspace 4 silent] kitty --title "btop" -e "btop"' &
 
 sleep 3 && echo -e "\nSetting colors for ${bold}waybar" &&
 pkill waybar && waybar &
 
-sleep 3 && echo -e "\nSetting colors for ${bold}cava" &&
+sleep 4 && echo -e "\nSetting colors for ${bold}cava" &&
 killall cava && kitty -c ~/.config/kitty/kittybg.conf --detach --class='kitty-bg' ~/.config/hypr/scripts/cava.sh &
 
 sleep 4 && echo -e "\nSetting colors for ${bold}swaync" &&
