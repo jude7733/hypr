5bc27d01cd5dba3142ddf600fa562a378e80a043
jude7733 <judeputhussery360@gmail.com>
2025-01-27 14:29:15 +0530

    minor changes

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
+
 # Battery status check
-BATTERY_STATUS=$(cat /sys/class/power_supply/BAT1/status)
+BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
 # Battery capacity check
-BATTETY_LEVEL=$(cat /sys/class/power_supply/BAT1/capacity)
+BATTERY_LEVEL=$(cat /sys/class/power_supply/BAT0/capacity)
 # Show notifications
-if [$BATTERY_STATUS -eq 'Discharging' && $BATTERY_LEVEL -le 15]; then
-notify-send -u critical "Battery critical!" "${BATTERY_LEVEL}%"
-paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
-elif [$BATTERY_STATUS -eq 'Charging' && $BATTERY_LEVEL -eq 100]; then
-notify-send -u critical "Battery full!" "${BATTERY_LEVEL}%"
-paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
-fi 
+if [ "$BATTERY_STATUS" = "Discharging" ] && [ "$BATTERY_LEVEL" -le 20 ]; then
+    notify-send -u critical "Battery critical!" "${BATTERY_LEVEL}%"
+    paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
+elif [ "$BATTERY_STATUS" = "Charging" ] && [ "$BATTERY_LEVEL" -eq 79 ]; then
+    notify-send -u critical "Battery full!" "${BATTERY_LEVEL}%"
+    paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
+fi
