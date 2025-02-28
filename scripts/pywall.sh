a5d3a54bdb839d7cb7d874dd9691465884aee19f
jude7733 <judeputhussery360@gmail.com>
2025-02-28 15:19:39 +0530

    fix unwanted waybar restart

--- scripts/pywall.sh
+++ scripts/pywall.sh
@@ -8,9 +8,6 @@ wal --cols16 -i "$wallpaper" && xrdb -merge ~/.cache/wal/colors.Xresources &
 sleep 3 && echo -e "\nSetting colors for ${bold}btop" && pkill btop && 
 hyprctl dispatch exec '[workspace 4 silent] kitty --title "btop" -e "btop"' &
 
-sleep 3 && echo -e "\nSetting colors for ${bold}waybar" &&
-pkill waybar && waybar &
-
 sleep 4 && echo -e "\nSetting colors for ${bold}cava" &&
 killall cava && kitty -c ~/.config/kitty/kittybg.conf --detach --class='kitty-bg' ~/.config/hypr/scripts/cava.sh &
 
