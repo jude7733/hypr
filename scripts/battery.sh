2ca9ad26ebc31bc90426b307b6cfede1ad481902
jude7733 <judeputhussery360@gmail.com>
2025-06-30 19:27:31 +0530

    tesseract bindings, animations

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -5,12 +5,13 @@ BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
 BATTERY_LEVEL=$(cat /sys/class/power_supply/BAT0/capacity)
 # Show notifications
 if [ "$BATTERY_STATUS" = "Discharging" ] && [ "$BATTERY_LEVEL" -le 20 ]; then
-    notify-send -u critical "Battery critical!" "${BATTERY_LEVEL}%"
-    paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
+  notify-send -u critical "Battery critical!" "${BATTERY_LEVEL}%"
+  brightnessctl set 10%
+  paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
 elif [ "$BATTERY_STATUS" = "Discharging" ] && [ "$BATTERY_LEVEL" -le 40 ]; then
-    notify-send -u normal "Time to charge!" "${BATTERY_LEVEL}%"
-    paplay /usr/share/sounds/freedesktop/stereo/dialog-information.oga
+  notify-send -u normal "Time to charge!" "${BATTERY_LEVEL}%"
+  paplay /usr/share/sounds/freedesktop/stereo/dialog-information.oga
 elif [ "$BATTERY_STATUS" = "Charging" ] && [ "$BATTERY_LEVEL" -eq 79 ]; then
-    notify-send -u critical "Battery full!" "${BATTERY_LEVEL}%"
-    paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
+  notify-send -u critical "Battery full!" "${BATTERY_LEVEL}%"
+  paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
 fi
--- scripts/pywall.sh
+++ scripts/pywall.sh
@@ -3,17 +3,14 @@
 wallpaper="$1"
 
 sleep 2 && echo -e "\nGenerating ${bold}pywal colorscheme from $wallpaper" &&
-wal --cols16 -i "$wallpaper" && xrdb -merge ~/.cache/wal/colors.Xresources &
+  wal --cols16 -n -i "$wallpaper" && xrdb -merge ~/.cache/wal/colors.Xresources &
 
-sleep 3 && echo -e "\nSetting colors for ${bold}btop" && pkill btop && 
-hyprctl dispatch exec '[workspace 4 silent] kitty --title "btop" -e "btop"' &
+# sleep 3 && echo -e "\nSetting colors for ${bold}btop" && pkill btop &&
+#   hyprctl dispatch exec '[workspace 4 silent] kitty --title "btop" -e "btop"' &
 
-sleep 4 && echo -e "\nSetting colors for ${bold}cava" &&
-killall cava && kitty -c ~/.config/kitty/kittybg.conf --detach --class='kitty-bg' ~/.config/hypr/scripts/cava.sh &
-
-sleep 4 && echo -e "\nSetting colors for ${bold}swaync" &&
-killall swaync && swaync &
+# sleep 4 && echo -e "\nSetting colors for ${bold}cava" &&
+#   killall cava && kitty -c ~/.config/kitty/kittybg.conf --detach --class='kitty-bg' ~/.config/hypr/scripts/cava.sh &
 
 bash ~/.config/hypr/scripts/battery.sh &
 
-pgrep -x "waybar" > /dev/null || waybar &
+pgrep -x "waybar" >/dev/null || waybar &
